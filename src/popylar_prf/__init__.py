164e99b8afed2387ecec335d8d3ab08cf5faaf1e
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2025-09-30 15:58:23 +0200

    Add Stimulus to __init__

--- src/popylar_prf/__init__.py
+++ src/popylar_prf/__init__.py
@@ -1,7 +1,13 @@
 """Documentation about popylar_prf."""
 
 import logging
+from popylar_prf.stimulus import Stimulus
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __version__ = "0.0.1"
+
+
+__all__ = [
+    "Stimulus",
+]
