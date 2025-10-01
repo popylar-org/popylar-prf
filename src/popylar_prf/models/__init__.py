0e82f440df0e9a85ed766a255e575af5803be4cc
maltelueken <malte_lueken@arcor.de>
2025-10-01 17:48:30 +0200

    Add 2D Gaussian pRF response model

--- src/popylar_prf/models/__init__.py
+++ src/popylar_prf/models/__init__.py
@@ -2,8 +2,10 @@
 
 from .base import BaseModel
 from .base import ResponseModel
+from .gaussian import Gaussian2DResponseModel
 
 __all__ = [
     "BaseModel",
+    "Gaussian2DResponseModel",
     "ResponseModel",
 ]
