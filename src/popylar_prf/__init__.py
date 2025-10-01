42de03ac1d2afb4c91c281f63bcbe543ec35dc85
maltelueken <malte_lueken@arcor.de>
2025-10-01 17:31:48 +0200

    Add utils module with pandas to tensor conversion function

--- src/popylar_prf/__init__.py
+++ src/popylar_prf/__init__.py
@@ -3,6 +3,7 @@
 import logging
 from popylar_prf.stimulus import Stimulus
 from popylar_prf.typing import Tensor
+from popylar_prf.utils import convert_parameters_to_tensor
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
@@ -12,4 +13,5 @@ __version__ = "0.0.1"
 __all__ = [
     "Stimulus",
     "Tensor",
+    "convert_parameters_to_tensor",
 ]
