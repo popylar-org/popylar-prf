2576ae5da1b091f1d58ecafc50ac8770dc737b1d
maltelueken <malte_lueken@arcor.de>
2025-10-01 17:31:13 +0200

    Add typing module with Tensor type

--- src/popylar_prf/__init__.py
+++ src/popylar_prf/__init__.py
@@ -2,6 +2,7 @@
 
 import logging
 from popylar_prf.stimulus import Stimulus
+from popylar_prf.typing import Tensor
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
@@ -10,4 +11,5 @@ __version__ = "0.0.1"
 
 __all__ = [
     "Stimulus",
+    "Tensor",
 ]
