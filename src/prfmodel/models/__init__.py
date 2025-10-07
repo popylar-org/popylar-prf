ff3c6ae6a820aecfd6f0e548164adb585c15245e
maltelueken <malte_lueken@arcor.de>
2025-10-07 16:38:18 +0200

    Add pRF encoding function

--- src/prfmodel/models/__init__.py
+++ src/prfmodel/models/__init__.py
@@ -2,8 +2,10 @@
 
 from .base import BaseModel
 from .base import ResponseModel
+from .encoding import encode_prf_response
 
 __all__ = [
     "BaseModel",
     "ResponseModel",
+    "encode_prf_response",
 ]
