f7403c961d8708a72f575bc21f9118bbeab50549
maltelueken <malte_lueken@arcor.de>
2025-10-09 15:25:51 +0200

    Add impulse response model base class

--- src/prfmodel/models/base.py
+++ src/prfmodel/models/base.py
@@ -4,6 +4,7 @@ from abc import ABC
 from abc import abstractmethod
 from collections.abc import Sequence
 import pandas as pd
+from keras import ops
 from prfmodel.stimulus import Stimulus
 from prfmodel.typing import Tensor
 
@@ -111,5 +112,79 @@ class BasePRFResponse(BaseModel):
         Tensor
             Model predictions of shape `(num_voxels, ...)`. The number of voxels is the number of rows in
             `parameters`. The number and size of other axes depends on the stimulus.
+
+        """
+
+
+class BaseImpulse(BaseModel):
+    """
+    Abstract base class for impulse response models.
+
+    Cannot be instantiated on its own.
+    Can only be used as a parent class to create custom impulse response models.
+    Subclasses must override the abstract `__call__` method.
+
+    #TODO: Link to Example on how to create custom impulse response models.
+
+    Parameters
+    ----------
+    duration : float, default=32.0
+        The duration of the impulse response (in seconds).
+    offset : float, default=0.0001
+        The offset of the impulse response (in seconds). By default a very small offset is added to prevent infinite
+        response values at t = 0.
+    resolution : float, default=1.0
+        The time resultion of the impulse response (in seconds), that is the number of points per second at which the
+        impulse response function is evaluated.
+    dtype : str, default="float64"
+        Dtype of the impulse response.
+
+    """
+
+    def __init__(
+        self,
+        duration: float = 32.0,
+        offset: float = 0.0001,
+        resolution: float = 1.0,
+        dtype: str = "float64",
+    ):
+        super().__init__()
+
+        self.duration = duration
+        self.offset = offset
+        self.resolution = resolution
+        self.dtype = dtype
+
+    @property
+    def num_frames(self) -> int:
+        """The total number of time frames at which the impulse response function is evaluated."""
+        return int(self.duration / self.resolution)
+
+    @property
+    def frames(self) -> Tensor:
+        """
+        The time frames at which the impulse response function is evaluated.
+
+        Time frames are linearly interpolated between `offset` and `duration` and have shape (1, `num_frames`).
+
+        """
+        return ops.expand_dims(ops.linspace(self.offset, self.duration, self.num_frames, dtype=self.dtype), 0)
+
+    @abstractmethod
+    def __call__(self, parameters: pd.DataFrame) -> Tensor:
+        """
+        Compute the impulse response.
+
+        Parameters
+        ----------
+        parameters : pandas.DataFrame
+            Dataframe with columns containing different model parameters and rows containing parameter values
+            for different voxels.
+
+        Returns
+        -------
+        Tensor
+            Model predictions of shape `(num_voxels, num_frames)`. The number of voxels is the number of rows in
+            `parameters`.
 
         """
