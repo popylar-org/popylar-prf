79c0ab7f48769a08ea26f292df47defe45690c3c
maltelueken <malte_lueken@arcor.de>
2025-10-16 16:49:20 +0200

    Add composite pRF model base class

--- src/prfmodel/models/__init__.py
+++ src/prfmodel/models/__init__.py
@@ -1,12 +1,14 @@
 """Population receptive field models."""
 
 from .base import BaseModel
+from .base import BasePRFModel
 from .base import BasePRFResponse
 from .base import BaseTemporal
 from .encoding import encode_prf_response
 
 __all__ = [
     "BaseModel",
+    "BasePRFModel",
     "BasePRFResponse",
     "BaseTemporal",
     "encode_prf_response",
